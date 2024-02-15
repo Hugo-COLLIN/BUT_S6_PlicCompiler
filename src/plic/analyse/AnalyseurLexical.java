a0a057b80584e75a80bae4c27114a189c0ec0285
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-02-15 09:08:44 +0100

    AnalyseurLexical

---
+++ src/plic/analyse/AnalyseurLexical.java
@@ -0,0 +1,31 @@
+package plic.analyse;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Adapte le comportement de Scanner et ignore les commentaires
+ */
+public class AnalyseurLexical {
+    Scanner scanner;
+    public AnalyseurLexical(File file) throws FileNotFoundException {
+        scanner = new Scanner(file);
+    }
+
+    public String next() {
+        if (!scanner.hasNext()) { // Check if there are more tokens available
+            return "";
+        }
+        String token = scanner.next();
+        if (token.equals("//")) {
+            scanner.nextLine();
+            return next();
+        }
+        return token;
+    }
+
+    public boolean hasNext() {
+        return scanner.hasNext();
+    }
+}
--- src/plic/tests/TestScanner.java
+++ src/plic/tests/TestScanner.java
@@ -1,19 +1,20 @@
 package plic.tests;
 
+import plic.analyse.AnalyseurLexical;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class TestScanner {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("C:\\Documents\\HugoC\\Git\\BUT\\BUT_S6_PLIC\\src\\plic\\sources\\test1.plic");
 
-        Scanner sc = new Scanner(file);
+//        Scanner sc = new Scanner(file);
+        AnalyseurLexical sc = new AnalyseurLexical(file);
 
         while (sc.hasNext()) {
             System.out.println(sc.next());
         }
-        sc.close();
-
     }
 }
