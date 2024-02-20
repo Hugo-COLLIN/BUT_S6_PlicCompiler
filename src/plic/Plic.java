bad1d2a43f42665176a4d2a231c32b93eabee335
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-02-20 13:15:50 +0100

    Plic error message when != 1 param + no message when correct parsing

--- src/plic/Plic.java
+++ src/plic/Plic.java
@@ -7,6 +7,19 @@ import plic.analyse.AnalyseurSyntaxique;
 
 public class Plic {
     public static void main(String[] args) throws FileNotFoundException {
+        if (args.length < 1) {
+            System.err.println("ERREUR: Chemin du fichier source absent");
+            System.exit(1);
+        }
+        if (args.length > 1) {
+            System.err.println("ERREUR: Un seul paramètre admis (chemin du fichier source)");
+            System.exit(1);
+        }
+        if (!args[0].endsWith(".plic")) {
+            System.err.println("ERREUR: Suffixe incorrect (fichier .plic attendu)");
+            System.exit(1);
+        }
+
         new Plic(args[0]);
     }
 
@@ -18,7 +31,6 @@ public class Plic {
         // Analyser le fichier
         try {
             anasynt.analyse();
-            System.out.println("Analyse syntaxique réussie");
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
