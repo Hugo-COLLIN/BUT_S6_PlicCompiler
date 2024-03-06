294df0fae775e37bbf37e6035b81266eb6dc6086
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-03-06 15:30:30 +0100

    tests: TDS reinitialiser dans beforeEach + nouveaux tests

--- src/plic/tests/repint/TDSTest.java
+++ src/plic/tests/repint/TDSTest.java
@@ -1,5 +1,6 @@
 package plic.tests.repint;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import plic.exceptions.DoubleDeclaration;
@@ -17,6 +18,7 @@ class TDSTest {
     @BeforeEach
     void setUp() {
         tds = TDS.getInstance();
+        tds.reinitialiser();
         entree = new Entree("testEntree");
         symbole = new Symbole("testSymbole");
     }
@@ -28,7 +30,7 @@ class TDSTest {
     }
 
     @Test
-    void testAjouter() throws DoubleDeclaration {
+    void testAjouter() {
         assertDoesNotThrow(() -> tds.ajouter(entree, symbole), "L'ajout d'une nouvelle entrée ne devrait pas lancer d'exception");
     }
 
@@ -36,5 +38,24 @@ class TDSTest {
     void testDoubleDeclaration() {
         assertDoesNotThrow(() -> tds.ajouter(entree, symbole), "Premier ajout devrait réussir");
         assertThrows(DoubleDeclaration.class, () -> tds.ajouter(entree, symbole), "Le deuxième ajout de la même entrée devrait lancer une DoubleDeclaration");
+    }
+
+    @Test
+    void testAjoutEtIdentification() throws DoubleDeclaration {
+        tds.ajouter(entree, symbole);
+
+        assertEquals(symbole, tds.identifier(entree), "Le symbole retourné doit correspondre à celui ajouté");
+    }
+
+    @Test
+    void testDeplacement() throws DoubleDeclaration {
+        tds.ajouter(entree, symbole);
+
+        Entree entree2 = new Entree("variable2");
+        Symbole symbole2 = new Symbole("entier");
+        tds.ajouter(entree2, symbole2);
+
+        assertEquals(-4, symbole.getDeplacement(), "Le déplacement de la première variable doit être -4");
+        assertEquals(-8, symbole2.getDeplacement(), "Le déplacement de la seconde variable doit être -8");
     }
 }
