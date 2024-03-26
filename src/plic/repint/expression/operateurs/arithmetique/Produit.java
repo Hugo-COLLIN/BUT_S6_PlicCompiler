43413a9566ba328cda44b5ecf1e56ad7a1978463
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-03-26 15:21:28 +0100

    fix: opérations arithmétiques utilisent la pile (fix problèmes de calcul pour les imbrications)

--- src/plic/repint/expression/operateurs/arithmetique/Produit.java
+++ src/plic/repint/expression/operateurs/arithmetique/Produit.java
@@ -10,10 +10,12 @@ public class Produit extends Arithmetique {
 
     @Override
     public String toMips() {
-        return opGauche.toMips() +      // Générer le code MIPS pour évaluer l'opérande gauche et le stocker dans $t0. Le résultat de l'addition est maintenant dans $v0, prêt à être utilisé ou stocké
-                "move $t0, $v0\n" +     // Sauvegarder le résultat de l'opérande gauche dans $t0
-                opDroit.toMips() +      // Générer le code MIPS pour évaluer l'opérande droit. À ce stade, le résultat de l'opérande droit est dans $v0
-                "mul $v0, $t0, $v0\n";  // Le résultat de la soustraction est maintenant dans $v0, prêt à être utilisé ou stocké
+        return opGauche.toMips() +
+                "sw $v0, 0($sp)\n" +
+                "addi $sp, $sp, -4\n" +
+                opDroit.toMips() +
+                "addi $sp, $sp, 4\n" +
+                "lw $t0, 0($sp)\n" +
+                "mul $v0, $t0, $v0\n";
     }
-    //TODO: bouger le registre $sp au lieu d'affceter à $v0
 }
--- src/plic/repint/expression/operateurs/arithmetique/Somme.java
+++ src/plic/repint/expression/operateurs/arithmetique/Somme.java
@@ -1,7 +1,6 @@
 package plic.repint.expression.operateurs.arithmetique;
 
 import plic.repint.expression.Expression;
-import plic.repint.expression.operateurs.Binaire;
 
 public class Somme extends Arithmetique {
     public Somme(Expression opGauche, Expression opDroit) {
@@ -10,9 +9,12 @@ public class Somme extends Arithmetique {
 
     @Override
     public String toMips() {
-        return opGauche.toMips() +      // Générer le code MIPS pour évaluer l'opérande gauche et le stocker dans $t0. Le résultat de l'addition est maintenant dans $v0, prêt à être utilisé ou stocké
-                "move $t0, $v0\n" +     // Sauvegarder le résultat de l'opérande gauche dans $t0
-                opDroit.toMips() +      // Générer le code MIPS pour évaluer l'opérande droit. À ce stade, le résultat de l'opérande droit est dans $v0
-                "add $v0, $t0, $v0\n";  // Le résultat de l'addition est maintenant dans $v0, prêt à être utilisé ou stocké
+        return opGauche.toMips() +
+                "sw $v0, 0($sp)\n" +
+                "addi $sp, $sp, -4\n" +
+                opDroit.toMips() +
+                "addi $sp, $sp, 4\n" +
+                "lw $t0, 0($sp)\n" +
+                "add $v0, $t0, $v0\n";
     }
 }
