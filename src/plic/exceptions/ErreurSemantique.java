4eaf2fc85094db5c46d420b92dbea7a95ac8ba49
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-04-04 11:01:39 +0200

    feat: Erreur précise si syntaxique ou semantique

--- src/plic/exceptions/ErreurSemantique.java
+++ src/plic/exceptions/ErreurSemantique.java
@@ -2,6 +2,6 @@ package plic.exceptions;
 
 public class ErreurSemantique extends Exception {
     public ErreurSemantique(String message) {
-        super("ERREUR: " + message);
+        super("ERREUR: " + message + " (Sémantique)");
     }
 }
--- src/plic/exceptions/ErreurSyntaxique.java
+++ src/plic/exceptions/ErreurSyntaxique.java
@@ -2,10 +2,10 @@ package plic.exceptions;
 
 public class ErreurSyntaxique extends Exception {
     public ErreurSyntaxique(String message) {
-        super("ERREUR: " + message);
+        super("ERREUR: " + message + " (Syntaxe)");
     }
 
     public ErreurSyntaxique(String message, String token) {
-        super("ERREUR: " + message + " - token \"" + token + "\"");
+        super("ERREUR: " + message + " - token \"" + token + "\"" + " (Syntaxe)");
     }
 }
